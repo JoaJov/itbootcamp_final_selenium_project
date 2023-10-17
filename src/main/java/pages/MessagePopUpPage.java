6afdf5d76d64a217ef348fbf53957858af24a360
JoaJov <jovanajovanovic493@gmail.com>
2023-10-17 19:35:22 +0100

    Verify Message Page

---
+++ src/main/java/pages/MessagePopUpPage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class MessagePopUpPage extends BasicPage {
+    public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+    public void waitForErrorPopupToBeVisible() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.v-snack__content")));
+    }
+
+    public String getErrorMessage() {
+        WebElement errorElement = driver.findElement(By.cssSelector("div > div.v-snack__content > ul > li"));
+        return errorElement.getText();
+    }
+    public WebElement getCloseButton () {
+        return driver.findElement(By.cssSelector(".v-card__actions button"));
+    }
+    public void clickOnCloseButton () {
+        getCloseButton().click();
+    }
+    public String getVerifyPopupMessage(){
+        WebElement verifyPopupText = driver.findElement(By.cssSelector(".black--text.dlgVerifyAccount"));
+        return verifyPopupText.getText();
+    }
+}
