850de1394003591bb796732b65d77c7e229d3ef5
JoaJov <jovanajovanovic493@gmail.com>
2023-10-19 20:09:56 +0100

    Verify forbids visits to admin cities URL if not authenticated

--- src/test/java/tests/AuthRoutesTest.java
+++ src/test/java/tests/AuthRoutesTest.java
@@ -16,5 +16,10 @@ public class AuthRoutesTest extends BasicTest {
         driver.get(baseUrl + "/profile");
         Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Current url should be " + baseUrl + "/login");
     }
+    @Test(priority = 3)
+    public void forbidVisitsToAdminCitiesUrlIfNotAuthenticated() throws InterruptedException {
+        driver.get(baseUrl + "/admin/cities");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Current url should be " + baseUrl + "/login");
+    }
 
 }
