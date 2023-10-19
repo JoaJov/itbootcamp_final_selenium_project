6edc39c003b2a8750a5a5ca5ed4ecadf400a6bd3
JoaJov <jovanajovanovic493@gmail.com>
2023-10-19 19:35:04 +0100

    Add test case Delete City in Admin Cities Tests

--- src/main/java/pages/MessagePopUpPage.java
+++ src/main/java/pages/MessagePopUpPage.java
@@ -7,10 +7,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.time.Duration;
+
 public class MessagePopUpPage extends BasicPage {
     public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
+
     public void waitForErrorPopupToBeVisible() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.v-snack__content")));
     }
@@ -19,14 +22,27 @@ public class MessagePopUpPage extends BasicPage {
         WebElement errorElement = driver.findElement(By.cssSelector("div > div.v-snack__content > ul > li"));
         return errorElement.getText();
     }
-    public WebElement getCloseButton () {
+
+    public WebElement getCloseButton() {
         return driver.findElement(By.cssSelector(".v-card__actions button"));
     }
-    public void clickOnCloseButton () {
+
+    public void clickOnCloseButton() {
         getCloseButton().click();
     }
-    public String getVerifyPopupMessage(){
+
+    public String getVerifyPopupMessage() {
         WebElement verifyPopupText = driver.findElement(By.cssSelector(".black--text.dlgVerifyAccount"));
         return verifyPopupText.getText();
+    }
+
+    public void waitForMessagePopupToBeVisible() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[3]/div/div/div/div")));
+    }
+
+    public String getMessagePopupText() {
+        WebElement messagePopup = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[3]/div/div/div/div/div[1]"));
+        return messagePopup.getText();
     }
 }
