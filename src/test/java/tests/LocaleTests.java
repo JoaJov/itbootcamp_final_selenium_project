521645a94f5beaf7246f60608fa5e1dcca0774b8
JoaJov <jovanajovanovic493@gmail.com>
2023-10-19 20:42:20 +0100

    Verify set locale to EN

--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import retryAnalyzer.RetryAnalyzer;
 
-public class LocaleTests extends BasicTest{
+public class LocaleTests extends BasicTest {
     @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
     public void setLocaleToES() {
 
@@ -13,5 +13,27 @@ public class LocaleTests extends BasicTest{
         navPage.clickOnSpanishLanguageInMenuList();
         Assert.assertEquals(navPage.getHeaderText(), "Página de aterrizaje",
                 "Text in header should be 'Página de aterrizaje'.");
+    }
+
+    @Test(priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void setLocaleToEN() {
+        navPage.clickOnLanguageMenuButton();
+        navPage.waitUntilLanguageListIsVisible();
+        navPage.clickOnEnglishLanguageInMenuList();
+        Assert.assertEquals(navPage.getHeaderText(), "Landing", "Text in header should be 'Landing'.");
+    }
+    @Test(priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void setLocaleToCN() {
+        navPage.clickOnLanguageMenuButton();
+        navPage.waitUntilLanguageListIsVisible();
+        navPage.clickOnChineseLanguageInMenuList();
+        Assert.assertEquals(navPage.getHeaderText(), "首页", "Text in header should be '首页'.");
+    }
+    @Test(priority = 4, retryAnalyzer = RetryAnalyzer.class)
+    public void setLocaleToFR() {
+        navPage.clickOnLanguageMenuButton();
+        navPage.waitUntilLanguageListIsVisible();
+        navPage.clickOnFrenchLanguageInMenuList();
+        Assert.assertEquals(navPage.getHeaderText(), "Page d'atterrissage", "Text in header should be 'Page d'atterrissage'.");
     }
 }
